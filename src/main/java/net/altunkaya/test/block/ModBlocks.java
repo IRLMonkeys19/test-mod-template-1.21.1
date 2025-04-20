9cc43ab38b5d426c43f0cd8733991b9825291d71
IRLMonkeys19 <altunkayaegemen@proton.me>
2025-04-20 13:58:54 +0200

    3 items and Blocks

---
+++ src/main/java/net/altunkaya/test/block/ModBlocks.java
@@ -0,0 +1,67 @@
+package net.altunkaya.test.block;
+
+import net.altunkaya.test.TestMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.enums.NoteBlockInstrument;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+
+public class ModBlocks {
+
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(3.0f)
+                    .mapColor(MapColor.PURPLE)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4.0f)
+                    .mapColor(MapColor.PINK)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE)));
+
+    public static final Block STEEL_BLOCK = registerBlock("steel_block",
+            new Block (AbstractBlock.Settings.create()
+                    .mapColor(MapColor.GRAY)
+                    .instrument(NoteBlockInstrument.IRON_XYLOPHONE)
+                    .requiresTool()
+                    .strength(6.0F, 6.0F)
+                    .sounds(BlockSoundGroup.METAL)));
+
+    private static Block registerBlock(String name, Block block){
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(TestMod.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block){
+        Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name),
+            new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        TestMod.LOGGER.info("registering Blocks for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries ->
+                fabricItemGroupEntries.add(ModBlocks.PINK_GARNET_BLOCK));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(fabricItemGroupEntries ->
+            fabricItemGroupEntries.add(ModBlocks.RAW_PINK_GARNET_BLOCK));
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries ->
+            fabricItemGroupEntries.add(ModBlocks.STEEL_BLOCK));
+
+
+    }
+}
