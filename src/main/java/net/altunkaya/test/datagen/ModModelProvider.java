eddd73e1fd67267fd592f1b49cdc7526442ca5fe
IRLMonkeys19 <altunkayaegemen@proton.me>
2025-04-20 22:07:21 +0200

    Custom interactable Items

---
+++ src/main/java/net/altunkaya/test/datagen/ModModelProvider.java
@@ -0,0 +1,39 @@
+package net.altunkaya.test.datagen;
+
+import net.altunkaya.test.block.ModBlocks;
+import net.altunkaya.test.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RAW_PINK_GARNET_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.STEEL_BLOCK);
+
+
+
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.PINK_GARNET, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RAW_PINK_GARNET, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.MAGIC_TORCH, Models.GENERATED);
+        itemModelGenerator.register(ModItems.LUCKY_STONE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.CHISEL, Models.GENERATED);
+
+    }
+}
